7131961fb3dd9c214844b47fbb50006b9be59049
jonathan delvig <delvigjon@hotmail.fr>
2024-10-06 20:07:25 +0200

    4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.limit(6) # Charge les 6 produits les plus populaires (exemple)
+    @products = Product.limit(4) # Charge les 6 produits les plus populaires (exemple)
   end
 end
